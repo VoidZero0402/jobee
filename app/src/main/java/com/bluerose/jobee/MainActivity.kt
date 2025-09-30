dd0c6036a1220743010ecc7709ce17964c7faa8b
VoidZero0402 <voidzero0204@gmail.com>
2025-09-30 13:41:57 +0330

    feat: create BaseActivity abstraction with auto inflate binding

--- app/src/main/java/com/bluerose/jobee/MainActivity.kt
+++ app/src/main/java/com/bluerose/jobee/MainActivity.kt
@@ -1,24 +1,8 @@
 package com.bluerose.jobee
 
-import android.content.res.Configuration
-import android.os.Bundle
-import androidx.activity.SystemBarStyle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowInsetsControllerCompat
+import com.bluerose.jobee.abstractions.BaseActivity
 import com.bluerose.jobee.databinding.ActivityMainBinding
-import com.bluerose.jobee.ui.utils.getThemeColor
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private val isSystemDarkMode: Boolean
-        get() = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge(navigationBarStyle = SystemBarStyle.dark(getThemeColor(R.attr.colorSurface)))
-        setContentView(binding.root)
-        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = !isSystemDarkMode
-    }
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun onViewCreated() {}
 }
---
+++ app/src/main/java/com/bluerose/jobee/abstractions/BaseActivity.kt
@@ -0,0 +1,38 @@
+package com.bluerose.jobee.abstractions
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.viewbinding.ViewBinding
+import com.bluerose.jobee.R
+import com.bluerose.jobee.ui.utils.getThemeColor
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+    protected lateinit var binding: VB
+    protected val isSystemDarkMode: Boolean
+        get() = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflateBinding()
+        enableEdgeToEdge(navigationBarStyle = SystemBarStyle.dark(getThemeColor(R.attr.colorSurface)))
+        setContentView(binding.root)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = !isSystemDarkMode
+        onViewCreated()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun inflateBinding(): VB {
+        val superclass = javaClass.genericSuperclass as ParameterizedType
+        val viewBindingClass = superclass.actualTypeArguments[0] as Class<VB>
+        val inflateMethod = viewBindingClass.getMethod("inflate", LayoutInflater::class.java)
+        return inflateMethod.invoke(null, layoutInflater) as VB
+    }
+
+    open fun onViewCreated() {}
+}
